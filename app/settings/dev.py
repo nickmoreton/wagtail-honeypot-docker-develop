fc3feeaa1704f3bfc06ced70191a2a010c2d1b6d
Nick Moreton <nickmoreton@me.com>
2022-05-16 16:19:23 +0000

    add initial wagtail app

---
+++ app/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-l^kxv1fkh9o4bc)djt46#vlbdbp9*tlk_72^jtcgbse!2u66*f"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
